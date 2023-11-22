8a89db0ee500dff4e9939f4308e2b0938a9b8e4a
Veronica Ni <vni1294@gmail.com>
2023-11-21 19:13:52 -0500

    implemented checkForWin

--- script/models.js
+++ script/models.js
@@ -85,6 +85,7 @@ class Game {
   */
   findSpotInCol(x) {
     console.log("starting findSpotInCol");
+    if (this.winner !== null) return;
     //check each row of the board at the index of x, starting from the bottom
     for (let y = this.numRows - 1; y >= 0; y--) {
       //if there is a 'null', replace with current player, return the [x,y] positon
@@ -111,29 +112,47 @@ class Game {
   */
   checkForWin() {
 
+    const _win = (coordinates) => {
+      //if some coordinates are not legal, return false
+      const illegalMove = coordinates.some(([y, x]) => {
+        return y < 0 || x < 0 || y >= this.numRows || x >= this.numCols
+      });
+
+      if (illegalMove) return false;
+      
+      //if all legal coordinates match current player, return true
+      const winningCombo = coordinates.every(([y, x]) => {
+        return this.board[y][x] === this.currPlayer.name;
+      });
+      console.log(`winningCombo: `, winningCombo);
+      
+      if (winningCombo) return true;
+    }
+
     //iterate over the board, via y, y < rowNum, y++
+    for (let y = 0; y < this.numRows; y++) {
       //iterate over the board via x, x < colNum, x++
+      for (let x = 0; x < this.numCols; x++) {
         //do a sweep of the board, starting from current y, and current x
         //assign LR, TP, DR, DL coords
-          //left to right: [[y][x], [y][x + 1], [y][x + 2], [y][x + 3]]
-          //top to bottom: board[y][x], board[y+1][x], board[y+2][x], board[y+3][x]
-          //diagonal right: board[y][x], board[y+1][x+1], board[y+2][x+2], board[y+3][x+3]
-          //diagonal left: board[y][x], board[y-1][x+1], board[y-2][x+2], board[y-3][x+3]
+        //left to right: [[y][x], [y][x + 1], [y][x + 2], [y][x + 3]]
+        const LR = [[y, x], [y, x + 1], [y, x + 2], [y, x + 3]];
+        //top to bottom: board[y][x], board[y+1][x], board[y+2][x], board[y+3][x]
+        const TB = [[y, x], [y + 1, x], [y + 2, x], [y + 3, x]];
+        //diagonal right: board[y][x], board[y+1][x+1], board[y+2][x+2], board[y+3][x+3]
+        const DR = [[y, x], [y + 1, x + 1], [y + 2, x + 2], [y + 3, x + 3]];
+        //diagonal left: board[y][x], board[y-1][x+1], board[y-2][x+2], board[y-3][x+3]
+        const DL = [[y, x], [y - 1, x + 1], [y - 2, x + 2], [y - 3, x + 3]];
         //if either (LR || TP || DL || DR) coords match the current player
+        if (_win(LR) || _win(TB) || _win(DR) || _win(DL)) {
           //update the game .winner to this. currentplayer
-    
-    function _win(coordinates) {
-      //[[0,0], [0,1], [0,2], [0,3]]
-      //e.g. [board[0][0], board[0][1], board[0][2], board[0][3], board[0][4]
-      //[null, p1, p1, p1]
-      //iterate over the array of coordinates
-        //map the coordindates to their values in the board
-      //if all values are the current Player
-        //return true
-      //return false;
+          this.winner = this.currPlayer;
+          return true;
+        }
+      }
     }
-    
-  }   
+    return false;
+  }
 
   // /** A function that triggers a sequence of events based on where the 
   //  *   current player chooses to place their piece.
